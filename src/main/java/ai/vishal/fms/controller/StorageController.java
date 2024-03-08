1ba5886d2e1c1198adbf9557966e1f125bfdc3a6
vishal2468 <vishalpoddar2468@gmail.com>
2024-03-08 18:54:33 +0530

    removed unsigned

--- src/main/java/ai/vishal/fms/controller/StorageController.java
+++ src/main/java/ai/vishal/fms/controller/StorageController.java
@@ -9,78 +9,14 @@ import org.springframework.stereotype.Controller;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class StorageController {
-
-  @GetMapping("/upload")
-  public ResponseEntity<String> uploadObject() throws IOException{
-    // The ID of your GCP project
-    String projectId = "fifth-diode-358105";
-
-    // The ID of your GCS bucket
-    String bucketName = "fms-test-bucket";
-
-    // The ID of your GCS object
-    String objectName = "artist.txt";
-
-    // The path to your file to upload
-    String filePath = "/Users/vishalpoddar/Desktop/artist.txt";
-
-    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
-    BlobId blobId = BlobId.of(bucketName, objectName);
-    BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
-
-    // Optional: set a generation-match precondition to avoid potential race
-    // conditions and data corruptions. The request returns a 412 error if the
-    // preconditions are not met.
-    Storage.BlobWriteOption precondition;
-    if (storage.get(bucketName, objectName) == null) {
-      // For a target object that does not yet exist, set the DoesNotExist precondition.
-      // This will cause the request to fail if the object is created before the request runs.
-      precondition = Storage.BlobWriteOption.doesNotExist();
-    } else {
-      // If the destination already exists in your bucket, instead set a generation-match
-      // precondition. This will cause the request to fail if the existing object's generation
-      // changes before the request runs.
-      precondition =
-          Storage.BlobWriteOption.generationMatch(
-              storage.get(bucketName, objectName).getGeneration());
-    }
-    storage.createFrom(blobInfo, Paths.get(filePath), precondition);
-
-    return
-            ResponseEntity.ok("File %s uploaded to bucket %s as %s".formatted(filePath, bucketName, objectName));
-  }
-
-  @GetMapping("/download")
-  public ResponseEntity<String> downloadObject() {
-    // The ID of your GCP project
-    String projectId = "fifth-diode-358105";
-
-    // The ID of your GCS bucket
-    String bucketName = "fms-test-bucket";
-
-    // The ID of your GCS object
-    String objectName = "artist1.txt";
-
-    // The path to which the file should be downloaded
-    String destFilePath = "/Users/vishalpoddar/vishal.txt";
-
-    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
-
-    Blob blob = storage.get(BlobId.of(bucketName, objectName));
-    blob.downloadTo(Paths.get(destFilePath));
-    return
-            ResponseEntity.ok("File %s downloaded from bucket %s as %s".formatted(destFilePath, bucketName, objectName));
-  }
 
   @GetMapping("/upload-signed-url")
   public ResponseEntity<String> generateV4PutObjectSignedUrl() throws StorageException, IOException {
@@ -152,8 +88,4 @@ public class StorageController {
     return ResponseEntity.ok("curl '" + url + "'");
   }
 
-  @GetMapping("/test")
-  public ResponseEntity<String> test() {
-    return ResponseEntity.ok("hi");
-  }
 }
